37c198d7c2df1f9fd61aa65d2421926e28872f2b
sherlockyzd <sherlockyzd1994@gmail.com>
2024-09-14 10:31:12 +0800

    调整工作目录

---
+++ example/cavity/pyFVMScript.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import time as time
+# 获取当前脚本文件所在的目录
+current_file_path = os.path.abspath(__file__)
+current_dir = os.path.dirname(current_file_path)
+# Add the path to the pyFVM directory
+src_path = os.path.abspath(os.path.join(current_dir, '..', '..', 'src'))
+print(f"src 路径: {src_path}")  # 打印路径，确保正确
+sys.path.insert(0, src_path)
+import pyFVM.Region as Region
+
+
+
+start=time.time()
+cfdCase=Region.Region(current_dir)
+cfdCase.RunCase()
+# region_vars=vars(cfdCase)
+print(time.time()-start)
